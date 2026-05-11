a884e02ece5936e9fe55565e12ec40aef8d7889b
Kaizenick <soham.deshpande2000@gmail.com>
2026-05-11 07:16:55 +0530

    added db CRUD for the endpoints

--- main.py
+++ main.py
@@ -33,46 +33,96 @@ def init_db():
 
 init_db()
 
+# ---------------------------------------------------------------------
 # get data from db
-# @app.get("/")
-# def get_all(db: Session = Depends(get_db)):
-#     db_products = db.query(database_models.Product).all()
-#     return db_products
-
-#get data from the objects 
 @app.get("/")
-def get_all():
-    return products
+def get_all(db: Session = Depends(get_db)):
+    db_products = db.query(database_models.Product).all()
+    return db_products
 
 #get data from the objects 
+# @app.get("/")
+# def get_all():
+#     return products
+
+# ---------------------------------------------------------------------
+#get data from db
 @app.get("/product/{id}")
-def get_product_by_id(id: int):
-    for product in products:
-        if product.id == id:
-            return product
-    
-    return "The product does not exist"
+def get_product_by_id(id: int, db: Session = Depends(get_db)):
+    product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if(product):
+        return product
+    else:
+        return "The product Does not Exist"
 
-#get data from the objects 
+# #get data from the objects 
+# @app.get("/product/{id}")
+# def get_product_by_id(id: int):
+#     for product in products:
+#         if product.id == id:
+#             return product
+    
+#     return "The product does not exist"
+
+# ---------------------------------------------------------------------
+
+# get data from db : add rows
 @app.post("/product/")
-def add_product(product: Product):
-    products.append(product)
-    return product
+def add_product(product: Product, db: Session = Depends(get_db)):
+    db.add(database_models.Product(**product.model_dump()))
+    db.commit()
+    return
+
+# #get data from the objects 
+# @app.post("/product/")
+# def add_product(product: Product):
+#     products.append(product)
+#     return product
+
+# ---------------------------------------------------------------------
+
+#get data from the db : update values 
+@app.post("/update/{id}")
+def update_product_info(id: int, name: str, price: float, db: Session = Depends(get_db)):
+    product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if(product):
+        product.name = name 
+        product.price = price
+
+        db.commit()
+        db.refresh(product)
+
+        return product
+    else:
+        return "The id does not Exist!!"
 
 #get data from the objects 
-@app.post("/update/{id}")
-def update_product_info(id: int, updatedproduct: Product):
-    for index,product in enumerate(products):
-        if(product.id == id):
-            products[index] = updatedproduct
-            return updatedproduct
-    return "Couldnt make changes, id not found!!"
-    
-#get data from the objects 
+# @app.post("/update/{id}")
+# def update_product_info(id: int, updatedproduct: Product):
+#     for index,product in enumerate(products):
+#         if(product.id == id):
+#             products[index] = updatedproduct
+#             return updatedproduct
+#     return "Couldnt make changes, id not found!!"
+
+# ---------------------------------------------------------------------
+#get data from the db : delete row
 @app.delete("/update/{id}")
-def delete_product_info(id: int):
-    for index,product in enumerate(products):
-        if(product.id == id):
-            del products[index]
-            return products
-    return "Couldnt make changes, id not found!!"  
+def delete_product_info(id: int, db: Session = Depends(get_db)):
+    product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if(product):
+        db.delete(product)
+        db.commit()
+        
+        return 
+    else:
+        raise ValueError("Data does not exist!!")
+
+#get data from the objects 
+# @app.delete("/update/{id}")
+# def delete_product_info(id: int):
+#     for index,product in enumerate(products):
+#         if(product.id == id):
+#             del products[index]
+#             return products
+#     return "Couldnt make changes, id not found!!"  
